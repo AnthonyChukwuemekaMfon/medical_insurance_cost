477f4384a430e582b4f6781ec444a8107636cac8
Anthony138839 <anthonychukwuemeka48@gmail.com>
2025-11-17 12:10:26 +0100

    complete initial project structure

---
+++ insurance_app.py
@@ -0,0 +1,50 @@
+# Importing dependencies
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Loading the saved model and columns
+saved_model = joblib.load(open("model.pkl", "rb"))
+X_columns = joblib.load(open("X_columns.pkl", "rb"))
+
+# Loading the dataset
+insurance_df = pd.read_csv("insurance.csv")
+
+# Creating a function for prediction
+def insurance_prediction(input_data):
+    # Define input data
+    input_data = (18,"female",26.315,0,"no","northeast")
+    # Converting to a dataframe
+    input_df = pd.DataFrame([input_data], columns=X_columns)
+    # Making predictions
+    prediction = saved_model.predict(input_df)
+    return prediction[0]
+
+def main():
+    # Giving a title for the app
+    st.title("Medical Insurance Cost Web App")
+    
+    # Getting user input
+    age = st.number_input("Age:", 18, 63, 19)
+    
+    sex_list = ["Male", "Female"]
+    sex = st.radio("Sex:", sex_list)
+    
+    bmi = st.slider("BMI:", 10.0, 50.0, 26.0, step=0.1)
+    
+    children = st.number_input("Number of children:", 0, 5, 1)
+    
+    smoker = st.radio("Do you smoke?", ["Yes", "No"])
+    
+    region_list = insurance_df["region"].unique().tolist()
+    region = st.selectbox("Select region:", region_list)
+    
+    # Code for prediction
+    insurance_pred = ""
+     # Creating a button to generate the predictive result
+    if st.button("Prediction result"):
+        insurance_pred = insurance_prediction([age, sex, bmi, children, smoker, region])
+        st.success(insurance_pred)
+    
+if __name__ == "__main__":
+    main()
